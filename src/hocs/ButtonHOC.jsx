6318b111eb361b894d247607fd1bb48f90b5230b
kostal79 <kostal1979@gmail.com>
2023-03-14 21:23:19 +0300

    refactored

--- src/hocs/ButtonHOC.jsx
+++ src/hocs/ButtonHOC.jsx
@@ -2,100 +2,97 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setDisplay,
+  setIsNewNumber,
   setOperator,
   setPreviosNumber,
 } from "../redux/slices/logicSlice";
 import { calculate } from "../utils/calculate/calculate";
 
 const ButtonHOC = (OriginalComponent) => {
-
   function NewComponent({ value }) {
     const mode = useSelector((state) => state.constr.mode);
     const display = useSelector((state) => state.logic.display);
     const dispatch = useDispatch();
     const cursor = mode === "Runtime" && "pointer";
     const operator = useSelector((state) => state.logic.operator);
-    const previosNumber = useSelector((state) => state.logic.previosNumber)
-  
+    const previosNumber = useSelector((state) => state.logic.previosNumber);
+    const isNewNumber = useSelector((state) => state.logic.isNewNumber);
+
     const numbersOnClick = (value) => {
+      value = String(value);
       if (mode === "Runtime") {
         if (value === "Clear") {
           dispatch(setDisplay("0"));
           dispatch(setPreviosNumber(null));
           dispatch(setOperator(null));
-        } else if (value === ",") {
-          if (display === "0") {
-            dispatch(setDisplay(`${display}${value}`));
-          } else if (!["+", "-", "*", "/", "Не определено"].includes(display)){
-            dispatch(
-              setDisplay(display.includes(value) ? display : display + value)
-            );
+          dispatch(setIsNewNumber(true));
+        } else if (isNewNumber) {
+          if (value === ",") {
+            dispatch(setDisplay("0,"));
+            dispatch(setIsNewNumber(false));
           } else {
-            return
+            dispatch(setDisplay(value));
+            dispatch(setIsNewNumber(false));
           }
-        } else {
-          if (["0", "+", "-", "*", "/", "Не определено"].includes(display)) {
-            if (!operator && !previosNumber && display === "-") {
-              dispatch(setDisplay(display + String(value)));
-            } else {
-              dispatch(setDisplay(String(value)));
-            }
-          } else {
+        } else if (!isNewNumber && display.length < 16) {
+          if (value === ",") {
             dispatch(
-              display.length < 16 ? setDisplay(display + String(value)) : display
+              setDisplay(display.includes(",") ? display : display + value)
             );
+          } else {
+            dispatch(setDisplay(display + value));
           }
         }
       }
     };
 
     const operatorOnClick = (currentOperator) => {
-        if (mode === "Runtime" && display !== "Не определено") {
-          if (!operator) {
-            if (currentOperator === "-" && parseFloat(display) === 0) {
-              dispatch(setDisplay("-"));
-            } else {
-              dispatch(setPreviosNumber(display));
-              dispatch(setOperator(currentOperator));
-              dispatch(setDisplay(currentOperator))
-            }
-          } else if (operator && previosNumber) {
-            dispatch(setDisplay(currentOperator));
-            dispatch(setPreviosNumber(calculate(previosNumber, display, currentOperator)))
-            dispatch(setOperator(currentOperator))
-          } else {
-            return
-          }
-        } else {
-          return;
+      if (mode === "Runtime" && display !== "Не определено") {
+        if (!operator && currentOperator === "-" && display === "0") {
+          dispatch(setDisplay("-"));
+          dispatch(setIsNewNumber(false));
         }
-      };
-
-      const equalOnClick = () => {
-        if (
-          !["+", "-", "*", "/"].includes(display) &&
-          mode === "Runtime" &&
-          previosNumber &&
-          display &&
-          operator
-        ) {
-          const ansver = calculate(previosNumber, display, operator);
-          dispatch(setDisplay(ansver));
-          dispatch(setOperator(null));
-          dispatch(setPreviosNumber(null));
+        if (!operator || operator === "=")  {
+          dispatch(setOperator(currentOperator));
+          dispatch(setPreviosNumber(display));
+          dispatch(setIsNewNumber(true));
+        } else if (!isNewNumber) {
+          const localAnsver = calculate(
+            previosNumber,
+            display,
+            operator
+          );
+          dispatch(setPreviosNumber(localAnsver));
+          dispatch(setOperator(currentOperator));
+          dispatch(setDisplay(localAnsver));
+          dispatch(setIsNewNumber(true));
         }
-        return;
-      };
+      }
+    };
 
-
+    const equalOnClick = () => {
+      if (
+        mode === "Runtime" &&
+        previosNumber &&
+        display !== "He определено" &&
+        operator
+      ) {
+        const finalAnsver = calculate(previosNumber, display, operator);
+        dispatch(setDisplay(finalAnsver));
+        dispatch(setOperator("="));
+        dispatch(setPreviosNumber(null));
+        dispatch(setIsNewNumber(true))
+      }
+      return;
+    };
 
     return (
       <OriginalComponent
         value={value}
         cursor={cursor}
         numbersOnClick={() => numbersOnClick(value)}
-        operatorOnClick={()=> operatorOnClick(value)}
-        equalOnClick = {equalOnClick}
+        operatorOnClick={() => operatorOnClick(value)}
+        equalOnClick={equalOnClick}
       />
     );
   }
--- src/redux/slices/logicSlice.js
+++ src/redux/slices/logicSlice.js
@@ -4,6 +4,7 @@ const initialState = {
     display: "0",
     previosNumber: null,
     operator: null,
+    isNewNumber: true,
 }
 
 export const logicSlice = createSlice({
@@ -13,10 +14,11 @@ export const logicSlice = createSlice({
         setDisplay: (state, action) => { state.display = action.payload },
         setPreviosNumber: (state, action) => { state.previosNumber = action.payload },
         setOperator: (state, action) => { state.operator = action.payload },
+        setIsNewNumber: (state, action) => { state.isNewNumber = action.payload },
         
     },
 })
 
-export const { setDisplay, setPreviosNumber, setOperator } = logicSlice.actions
+export const { setDisplay, setPreviosNumber, setOperator, setIsNewNumber } = logicSlice.actions
 
 export default logicSlice.reducer
